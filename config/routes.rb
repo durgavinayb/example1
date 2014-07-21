25662ae4eb9f2a03bc9609ac248ef9b420436468
Durga Vinay Kumar. B <dkumarb@groupon.com>
2014-07-22 01:34:03 +0530

    Finish user signup--Add SSL in production

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Workshop::Application.routes.draw do
-  get "users/new"
+  resources :users
+
   root to: 'static_pages#home'
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
